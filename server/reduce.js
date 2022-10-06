322a5aba87aa5233ec6e30d842ea04a5bd093236
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-06 12:09:47 -0500

    clase11

--- server/reduce.js
+++ server/reduce.js
@@ -14,3 +14,18 @@ const reduce = [3,5,6,7,8];
 let suma = 0;
 const rpt = reduce.reduce((suma, item) => suma + item, 0);
 console.log('rpt', rpt);
+
+
+//  reto
+
+const tres = [1, 1, 1];
+const catorce = [2, 4, 8];
+
+function funsuma(tres) {
+    return tres.reduce((sum, item) => sum + item, 0);
+}
+
+const tress = funsuma(tres);
+const catorces = funsuma(catorce);
+console.log(tress, catorces);
+
