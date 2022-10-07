cc8b1fb3052b9cde7ede28e5063232bcd36830ff
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-07 14:02:59 -0500

    clase20

--- server/join-split.js
+++ server/join-split.js
@@ -2,7 +2,8 @@
 
 
 
-
+// join es para unificar de acuerdo a un patron 
+// split para separar de acuerdo a un patron
 const elements = ['fire', 'water', 'air','earth'];
 
 let rsp = '';
@@ -16,5 +17,11 @@ for (let i = 0; i < elements.length; i++) {
 const joinrsp = elements.join(' & ');
 
 
+//Split  
+const title ="The last of us";
+
+const urlgame = title.split(' ').join(' & ').toLowerCase();
+
+console.log('url juego con split', urlgame);
 console.log('con for un join:', rsp);
 console.log('con metodo join:', joinrsp);
