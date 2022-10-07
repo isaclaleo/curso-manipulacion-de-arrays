e1ff9d5c4c94c8412f121ab92ea8a2feb1d93ee0
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-07 14:23:08 -0500

    claase 21 con reto

--- server/join-split.js
+++ server/join-split.js
@@ -1,3 +1,5 @@
+// El método join une los elementos del array, mediante una separación, y retorna un string. Si un elemento es undefined o null, se convierte en una cadena vacía.
+
 //join es unirlo por un separador en especifico
 
 
@@ -25,3 +27,30 @@ const urlgame = title.split(' ').join(' & ').toLowerCase();
 console.log('url juego con split', urlgame);
 console.log('con for un join:', rsp);
 console.log('con metodo join:', joinrsp);
+
+// Clas 21 Reto
+
+const solution1 = "La forma de correr Python";
+const solution2 = "La API para nunca parar de aprender";
+const solution3 = "Curso de arrays";
+
+// Con una function
+function funtjoin(solution1) {
+    return solution1.split(" ").join("-").toLowerCase();
+}
+
+const oracion1 = funtjoin(solution1);
+const oracion2 = funtjoin(solution2);
+const oracion3 = funtjoin(solution3);
+console.log('Primera oracion', oracion1);
+console.log('Segunda oracion', oracion2);
+console.log('Tercera oracion', oracion3);
+
+//Con metodo de una linea 
+const frase1 = solution1.split(' ').join('-').toLowerCase();
+const frase2 = solution2.split(' ').join('-').toLowerCase();
+const frase3 = solution3.split(' ').join('-').toLowerCase();
+
+console.log('frase 1 oracion', frase1);
+console.log('frase 2 oracion', frase2);
+console.log('frase 3 oracion', frase3);
